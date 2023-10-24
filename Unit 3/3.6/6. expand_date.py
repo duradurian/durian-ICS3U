3880f775af4c837190a32258b441ea09499370c0
duradurian <113811889+duradurian@users.noreply.github.com>
2023-10-24 18:00:11 -0400

    updte for q7

--- Unit 3/3.6/6. expand_date.py
+++ Unit 3/3.6/6. expand_date.py
@@ -7,12 +7,14 @@ Discription: Expand date function.
 def expand_date(input_date):
     '''Converts mm/dd/yyyy to regular form: eg. September, 12, 2023'''
 
+    # Corresponding number and word.
     month_dictionary = [(1, "January"),(2, "Febuary"),(3, "March"),(4, "April"),(5, "May"),(6, "June"),(7, "July"),(8, "August"),(9, "September"),(10, "October"),(11, "November"),(12, "December")]
 
+    # Split input date into list, then convert numerical month to word.
     list_date = input_date.split("/")
-
     month = month_dictionary[int(list_date[0])-1][1]
-
+    
+    # Return month in word format along with date and year
     return f"{month}, {list_date[1]}, {list_date[2]}"
 
 def main():
---
+++ Unit 3/3.6/7. average_number_words.py
@@ -0,0 +1,5 @@
+"""
+Author: Darrien Guan
+Date: Oct 24
+Discription: Calculates the average number of words per sentence in a text file.
+"""

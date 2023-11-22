95d7ebb0f2a4492b8029f4c9fac49914fb7ea9f7
duradurian <113811889+duradurian@users.noreply.github.com>
2023-11-21 20:27:03 -0500

    new

--- Unit 4/4.4/7. student_class.py
+++ Unit 4/4.4/7. student_class.py
@@ -6,10 +6,12 @@ Discription: Student subclass
 
 class Person():
     def __init__(self, name, address, telephone):
+        '''Set object variables.'''
         self.__name = name
         self.__address = address
         self.__telephone = telephone
 
+    # Mutator methods
     def get_name(self):
         return self.__name
 
@@ -19,6 +21,7 @@ class Person():
     def get_telephone(self):
         return self.__telephone
     
+    # Accessor methods
     def set_name(self, new_name):
         self.__name = new_name
 
@@ -30,18 +33,22 @@ class Person():
 
 class Student(Person):
 
-    def __init__(self, cellphone = False, student_id = 0, name = None, address = None, telephone = None):
-        Person.__init__(name, address, telephone)
+    def __init__(self, cellphone = False, student_id = 0, name = "NA", address = "NA", telephone = 0):
+        '''Initalize student class as subclass of Person class.'''
+        Person.__init__(self, name, address, telephone)
 
+        # Set object variables
         self.__has_cellphone = cellphone
         self.__student_id = student_id
 
+    # Mutator methods
     def is_cellphone(self):
         return self.__has_cellphone
     
     def get_student_id(self):
         return self.__student_id
     
+    # Accessor methods
     def set_has_cellphone(self, cellphone = False):
         self.__has_cellphone = cellphone
 
@@ -50,7 +57,17 @@ class Student(Person):
 
 def main():
     '''main logic'''
-
+    
+    # Initialize student object instance
     student_instance = Student(True, 348592924, "Darrien", "7522 Kennedy Road", "123123123123")
+    
+    # Display object instance variables.
+    print("Student status:")
+    print("Name:", student_instance.get_name())
+    print("Address:", student_instance.get_address())
+    print("Student ID:", student_instance.get_student_id())
+    print("Telephone:", student_instance.get_telephone())
+    print("Has cellphone:", student_instance.is_cellphone())
+    
 
 main()

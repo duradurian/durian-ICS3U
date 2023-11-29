9c738305bcb67af360ac8f92f6705e562255fbd0
duradurian <113811889+duradurian@users.noreply.github.com>
2023-11-29 11:21:01 -0500

    yes

--- Unit 5/5.2/self_portrait.py
+++ Unit 5/5.2/self_portrait.py
@@ -1,14 +1,25 @@
 import pygame
-import math
+pygame.init()
 
 def drawStuff(background):
     """ given a Surface, this function draws a bunch of
     shapes on it """
 
-    # draw polygon
-    points = ((137, 372), (232, 319), (383, 335), (442, 389),
-              (347, 432), (259, 379), (220, 439), (132, 392))
-    pygame.draw.polygon(background, (0x33, 0xFF, 0x33), points)
+    # hair coords
+    hair_points = ((201, 70),(214, 148),(224, 92),(238, 147),(259, 93),
+              (273, 148),(287, 109),(297, 155),(313, 112),(334, 149),(356, 117),(385, 146),
+              (417, 99),(409, 40),(381, 24),(308, 18),(255, 19),(169, 43),(170, 100),(177, 159))
+    
+    # Mouth coords
+    coordinates = (
+                (231, 217),(234, 223),(241, 227),(246, 229),(258, 229),(268, 229),
+                (276, 229),(283, 228),(293, 226),(301, 224),(304, 223))
+
+    pygame.draw.circle(background, (210,180,100), (295, 159), 125)
+    pygame.draw.circle(background, (0, 0, 0), (238, 157), 10)
+    pygame.draw.circle(background, (0, 0, 0), (295, 160), 10)
+    pygame.draw.polygon(background, (0, 0, 0), hair_points)
+    pygame.draw.polygon(background, (0, 0, 0), coordinates)
 
 def main():
     # Initialize Pygame
